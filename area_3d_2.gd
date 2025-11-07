cf857ae6691148633685b163e6bca8ffef557d2c
Nicholas Shen <239685876+nshen0009@users.noreply.github.com>
2025-11-07 15:38:39 -0800

    revert

--- area_3d_2.gd
+++ area_3d_2.gd
@@ -1,10 +1,11 @@
-extends Node3D
+extends Area3D
 
 func _ready():
-	pass
+	body_entered.connect(_on_body_entered)
+
 func _on_body_entered(body):
 	# Check if the player is the one touching
 	print("hi")
-	if body.is_in_group("player"):
+	if body.name == CharacterBody3D:
 		print("Coin collected!")
 		queue_free()  # Remove coin from scene
